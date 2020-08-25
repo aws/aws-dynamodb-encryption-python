3c404b3bf4ae169259155a2250d04a707a08e867
mattsb42-aws <bullocm@amazon.com>
2020-08-25 14:10:18 -0700

    chore: fix sourcebuildcheck script - The ls command was getting a relative path when the script ran but a bare filename in manual checks.

--- test/source-build-check.sh
+++ test/source-build-check.sh
@@ -4,20 +4,26 @@
 #NOTE: Humans should not run this file directly. If you want to run this check, use the
 #   tox to run the "sourcebuildcheck" test environment.
 
-WORKINGDIR=$1
-DISTDIR=$2
+WORKINGDIR=${1}
+DISTDIR=${2}
+
+echo "Performing source build check"
+echo "Using working directory ${WORKINGDIR}"
+echo "Using dist directory ${DISTDIR}"
 
 echo "Locating the source build and copying it into the working directory."
-DISTFILE=`ls $DISTDIR/dynamodb-encryption-sdk-*.tar.gz | tail -1`
-cp $DISTFILE $WORKINGDIR
-DISTFILE=`ls $WORKINGDIR/dynamodb-encryption-sdk-*.tar.gz | tail -1`
+DISTFILE=$(ls ${DISTDIR}/dynamodb-encryption-sdk-*.tar.gz | tail -1)
+echo "Found source build at ${DISTFILE}"
+cp ${DISTFILE} ${WORKINGDIR}
 
 echo "Extracting the source build."
-cd $WORKINGDIR
-tar xzvf $DISTFILE
-rm $DISTFILE
-EXTRACTEDDIR=`ls | tail -1`
-cd $EXTRACTEDDIR
+cd ${WORKINGDIR}
+NEWDISTFILE=$(ls dynamodb-encryption-sdk-*.tar.gz | tail -1)
+echo "Using distfile ${NEWDISTFILE}"
+tar xzvf ${NEWDISTFILE}
+rm ${NEWDISTFILE}
+EXTRACTEDDIR=$(ls | tail -1)
+cd ${EXTRACTEDDIR}
 
 echo "Installing requirements from extracted source build."
 pip install -r test/requirements.txt
