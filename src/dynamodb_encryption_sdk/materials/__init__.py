4acc0ae7dd8e62a9722536b5de6cc1619e8fed5b
KhubaibAlam <74249716+khubaibalam2000@users.noreply.github.com>
2023-09-25 23:13:10 +0500

    fixed-import-position-in-materials-L19

--- src/dynamodb_encryption_sdk/materials/__init__.py
+++ src/dynamodb_encryption_sdk/materials/__init__.py
@@ -16,9 +16,9 @@ from typing import Dict, Text
 
 import six
 
-from mypy_extensions import NoReturn
-
 from dynamodb_encryption_sdk.delegated_keys import DelegatedKey  # noqa pylint: disable=unused-import
+
+from mypy_extensions import NoReturn
 
 __all__ = ("CryptographicMaterials", "EncryptionMaterials", "DecryptionMaterials")
 
